dfc0622cb784184c459df8253bef47d5584fc027
Marinelle <nellebreyes@gmail.com>
2021-08-09 00:13:44 -0400

    added video element

---
+++ js/quotes.js
@@ -0,0 +1,70 @@
+const localQuotes = [
+    {
+        quote: "The greatest marriages are built on teamwork. A mutual respect, a healthy dose of admiration, and a never-ending portion of love and grace.",
+        author: "Fawn Weaver"
+    },
+    {
+        quote:"A successful marriage requires falling in love many times, always with the same person.",
+        author:"Mignon McLaughlin"
+    },
+    {
+        quote:"I love your feet because they have wandered over the earth and through the wind and water until they brought you to me.",
+        author:"Pablo Neruda"
+    },
+    {
+        quote:"A happy marriage is the union of two good forgivers.",
+        author:"Robert Quillen"
+    },
+    {
+        quote:"Coming together is a beginning; keeping together is progress; working together is success.",
+        author:"Henry Ford"
+    },
+    {
+        quote:"Love may be blind, but marriage is a real eye-opener.",
+        author:"Anonymous"
+    },
+    {
+        quote:"I have learned that only two things are necessary to keep one’s wife happy. First, let her think she’s having her own way. And second, let her have it.",
+        author:"Lyndon B. Johnson"
+    },
+    {
+        quote:"Love doesn’t make the world go round. Love is what makes the ride worthwhile.",
+        author:"Franklin P. Jones"
+    },
+    {
+        quote:"What I love most about my home is who I share it with.",
+        author:"Tad Carpenter"
+    },
+    {
+        quote:"I love you not only for what you are, but for what I am when I am with you.",
+        author:"Elizabeth Barrett Browning"
+    },
+    {
+        quote:"Love is the thing that is always having the last word—even when those words are 'I'm sorry,' or 'Forgive me,' or 'I love you no matter what.",
+        author:"Cleo Wade"
+    },
+    {
+        quote:"For it was not into my ear you whispered, but into my heart. It was not my lips you kissed, but my soul.",
+        author:"Judy Garland"
+    },
+    {
+        quote:"Being deeply loved by someone gives you strength, While loving someone deeply gives you courage.",
+        author:"Lao Tzu"
+    },
+    {
+        quote:"For all the things my hands have held, the best by far is you.",
+        author:"Andrew McMahon"
+    },
+    {
+        quote:"The very essence of romance is uncertainty.",
+        author:"Oscar Wilde"
+    },
+    {
+        quote:"If you live to be 100, I hope I live to be 100 minus one day, so I never have to live without you.",
+        author:"Ernest H. Shepard"
+    },
+    {
+        quote:"Once in awhile, right in the middle of an ordinary life, love gives us a fairy tale.",
+        author:"Anonymous"
+    },
+]
--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,32 @@
-document.querySelector(".rings");
+
+//Reviews
+let randomQuotes = localQuotes;
+let oneQuote = [];
+
+var reviewBtn = document.querySelector('.reviewBtn');
+
+function showReview(){
+     var quote = document.querySelector('.quote span');
+     var quoteAuthor = document.querySelector('.quote .reviewAuthor');
+     oneQuote = randomQuotes[Math.floor(Math.random() * randomQuotes.length)];
+     quote.innerHTML = oneQuote.quote;
+     quoteAuthor.innerHTML = oneQuote.author;
+}
+
+showReview();
+
+reviewBtn.addEventListener('click', showReview);
+
+// if checkbox is checked and main is clicked 
+
+var navigation = document.querySelector('.navigation');
+var main = document.querySelector('.main');
+function hideNavigation(){
+     if(document.querySelector('.checkbox').checked){
+          document.querySelector('.checkbox').checked = false;
+     }
+}
+hideNavigation();
+main.addEventListener('click',hideNavigation);
+
+// end of if checkbox is checked
